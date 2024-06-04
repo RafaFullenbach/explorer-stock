f6275b94a42dfcd0be54663903e8cd2e8c52c2e8
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2024-06-04 19:37:21 -0300

    feat: finish

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,16 +1,41 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 import { useAuth } from "../hooks/auth";
+import { USER_ROLE } from "../utils/role";
 
-import { AppRoutes } from './app.routes';
-import { AuthRoutes } from './auth.routes';
+import { AuthRoutes } from "./auth.routes";
+
+import { AdminRoutes } from "./admin.routes";
+import { SaleRoutes } from "./sale.routes";
+import { CustomerRoutes } from "./customer.routes";
+import { useEffect } from "react";
+import { api } from "../services/api";
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api.get("/users/validated").catch((error) => {
+      if (error.response?.status === 401) {
+        signOut();
+      }
+    });
+  }, []);
+
+  function AcessRoute() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.SALE:
+        return <SaleRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      default:
+        return <CustomerRoutes />;
+    }
+  }
 
   return (
-    <BrowserRouter>
-      {user ? <AppRoutes /> : <AuthRoutes />}
-    </BrowserRouter>
+    <BrowserRouter>{user ? <AcessRoute /> : <AuthRoutes />}</BrowserRouter>
   );
 }
